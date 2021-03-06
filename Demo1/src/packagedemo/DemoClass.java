a9f921f131ae35075557f2a0435f6c70afc337d9
Dishita210 <80039953+Dishita210@users.noreply.github.com>
2021-03-06 16:49:03 +0530

    Update DemoClass.java

--- Demo1/src/packagedemo/DemoClass.java
+++ Demo1/src/packagedemo/DemoClass.java
@@ -6,6 +6,7 @@ public class DemoClass {
 		// TODO Auto-generated method stub
 		System.out.println("main Branch");
 		System.out.println("Demo Branch2");
+		System.out.println("Demo Branch2 git url changes");
 	}
 
 }
